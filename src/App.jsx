2174d553b8dfa9204ce4dea2510aec493899df31
Jenna McLaughlin <89791791+jennagmclaughlin@users.noreply.github.com>
2024-11-26 01:46:36 -0500

    added three.js boilerplate

--- src/App.jsx
+++ src/App.jsx
@@ -1,169 +1,84 @@
-import { useEffect, useRef, useState } from 'react';
+import { useEffect } from 'react';
+
 import * as THREE from 'three';
-import './App.css';
 import vertexShaderSky from './shaders/sky/vertexShader.glsl';
 import fragmentShaderSky from './shaders/sky/fragmentShader.glsl';
 
-function App() {
-  const canvasRef = useRef(null);
-  const [isDragging, setIsDragging] = useState(false); // track if the user is dragging the mouse
+import SceneInit from './lib/SceneInit';
 
+function App() {
   useEffect(() => {
-    const scene = new THREE.Scene();
-    scene.background = new THREE.Color().setHSL( 0.6, 0, 1 );
-    scene.fog = new THREE.Fog(scene.background, 1, 5000);
-    const camera = new THREE.PerspectiveCamera(
-      120, // field of view
-      window.innerWidth / window.innerHeight, // aspect ratio
-      1, // near plane
-      1000 // far plane
-    );
-    camera.position.z = 50;
-    const loadTexture = new THREE.TextureLoader();
-    loadTexture.load('./textures/wave-pattern.png');
-    // set up the WebGL renderer
-    const renderer = new THREE.WebGLRenderer({
-      canvas: canvasRef.current,
-      antialias: true,
-      alpha: true
-    });
-    renderer.setSize(window.innerWidth, window.innerHeight);
-    renderer.setPixelRatio(window.devicePixelRatio);
-    renderer.shadowMap.enabled = true;
-    document.body.appendChild(renderer.domElement); // putting renderer in document
+    const test = new SceneInit('myThreeJsCanvas');
+    test.initialize();
+    test.animate();
 
     const hemisphere = new THREE.HemisphereLight(0xffffff, 0xffffff, 2);
     hemisphere.color.setHSL(0.6, 1, 0.6);
     hemisphere.groundColor.setHSL(0.095, 1, 0.75);
     hemisphere.position.set(0, 1, 0);
-    scene.add(hemisphere);
+    test.scene.add(hemisphere);
 
-    // main light
-    const sun = new THREE.DirectionalLight(0xffffff, 2);
-    sun.position.set();
-    sun.castShadow = true;
-    scene.add(sun);
+     // main light
+     const sun = new THREE.DirectionalLight(0xffffff, 2);
+     sun.position.set();
+     sun.castShadow = true;
+     test.scene.add(sun);
 
-    // ambient light
-    const ambientLight = new THREE.AmbientLight(0xffffff, 0.5);
-    scene.add(ambientLight);
-
-    const dirLight = new THREE.DirectionalLight( 0xffffff, 0 );
-    dirLight.color.setHSL( 0.1, 1, 0.95 );
-    dirLight.position.set( -1, 1.75, 1 );
-    dirLight.position.multiplyScalar( 30 );
-    scene.add( dirLight );
-
-    dirLight.castShadow = true;
-
-    dirLight.shadow.mapSize.width = 2048;
-    dirLight.shadow.mapSize.height = 2048;
-
-    const d = 50;
-
-    dirLight.shadow.camera.left = - d;
-    dirLight.shadow.camera.right = d;
-    dirLight.shadow.camera.top = d;
-    dirLight.shadow.camera.bottom = - d;
-
-    dirLight.shadow.camera.far = 500;
-    dirLight.shadow.bias = -0.0001;
-
-    const uniforms = {
+    const skyUniforms = {
       'topColor': { value: new THREE.Color( 0x0077ff ) },
       'bottomColor': { value: new THREE.Color( 0xffffff ) },
       'offset': { value: 33 },
-      'exponent': { value: 0.5 }
+      'exponent': { value: 0.6 }
     };
-    uniforms[ 'topColor' ].value.copy( hemisphere.color );
+    skyUniforms[ 'topColor' ].value.copy( hemisphere.color );
 
-    scene.fog.color.copy( uniforms[ 'bottomColor' ].value );
+    test.scene.fog = new THREE.Fog(test.background, 1, 5000);
+    test.scene.fog.color.copy( skyUniforms[ 'bottomColor' ].value );
 
     const skyGeo = new THREE.SphereGeometry( 300, 100, 100 );
     const skyMat = new THREE.ShaderMaterial( {
-      uniforms: uniforms,
+      uniforms: skyUniforms,
       vertexShader: vertexShaderSky,
       fragmentShader: fragmentShaderSky,
       side: THREE.BackSide
     } );
 
     const sky = new THREE.Mesh( skyGeo, skyMat );
-    scene.add(sky);
+    test.scene.add(sky);
 
-    // plane geometry
-    const planeGeometry = new THREE.PlaneGeometry(100, 100, 32, 32); // Added more segments for smoother wave
-
-    // material for the plane
-    const planeMaterial = new THREE.MeshBasicMaterial({ color: 0x000000, wireframe: true });
-    const planeMesh = new THREE.Mesh(planeGeometry, planeMaterial);
-    planeMesh.castShadow = true;
-    planeMesh.rotateX(200);
-    scene.add(planeMesh);
-
-    // mouse movement and dragging
-    let mouseX = 0;
-    let mouseY = 0;
-
-    // update mouse position
-    const onMouseMove = (event) => {
-      mouseX = (event.clientX / window.innerWidth) * 2 - 1;
-      mouseY = -(event.clientY / window.innerHeight) * 2 + 1;
-      if (isDragging) {
-        applyWaveEffect(mouseX, mouseY); 
-      }
-    };
-
-    const onMouseDown = () => {
-      setIsDragging(true);
-    };
-
-    const onMouseUp = () => {
-      setIsDragging(false);
-    };
-
-    //wave effect to the plane based on mouse position
-    const applyWaveEffect = (mouseX, mouseY) => {
-      const time = Date.now() * 0.002; 
-      const vertices = planeGeometry.attributes.position.array;
-
-      // wave effect based on mouse position
-      for (let i = 0; i < vertices.length; i += 3) {
-        const x = vertices[i];
-        const z = vertices[i + 2];
-
-  
-        const distance = Math.sqrt(Math.pow(mouseX - (x / 100), 2) + Math.pow(mouseY - (z / 100), 2)); // adjust wave radius
-        if (distance < 0.3) { 
-          vertices[i + 1] = Math.sin(x * 0.1 + time) * 12 + Math.sin(z * 0.1 + time) * 12; // wave amp
-        }
-      }
-
-      planeGeometry.attributes.position.needsUpdate = true; 
-    };
-
-    // mouse event listeners
-    window.addEventListener('mousemove', onMouseMove);
-    window.addEventListener('mousedown', onMouseDown);
-    window.addEventListener('mouseup', onMouseUp);
-
-    // window resizing
-    window.addEventListener('resize', onWindowResize, false);
-    function onWindowResize() {
-      camera.aspect = window.innerWidth / window.innerHeight;
-      camera.updateProjectionMatrix();
-      renderer.setSize(window.innerWidth, window.innerHeight);
+    function vertexShader() {
+      return `
+          varying float z;
+          uniform float u_time;
+          void main() {
+            z = (cos(position.y + u_time) + sin(position.x + u_time)) / 4.0;
+            gl_Position = projectionMatrix * modelViewMatrix * vec4(position.x, position.y, z + position.z, 1.0);
+          }
+        `;
     }
-
-    // animation loop
-    function animate() {
-      renderer.render(scene, camera);
-      requestAnimationFrame(animate); 
+    function fragmentShader() {
+      return `
+          void main() {
+            gl_FragColor = vec4(1, 0, 0, 1);
+          }
+        `;
     }
-    animate(); 
+    const geometry = new THREE.BoxGeometry(1, 1, 1, 1, 1, 1);
+    const material = new THREE.ShaderMaterial({
+      uniforms: test.uniforms,
+      fragmentShader: fragmentShader(),
+      vertexShader: vertexShader(),
+    });
+    const mesh = new THREE.Mesh(geometry, material);
+    mesh.rotation.x = Math.PI / 2;
+    test.scene.add(mesh);
+  }, []);
 
-  }, [isDragging]); 
-
-  return <canvas ref={canvasRef} />;
+  return (
+    <div>
+      <canvas id="myThreeJsCanvas" />
+    </div>
+  );
 }
 
 export default App;
--- vite.config.js
+++ vite.config.js
@@ -1,32 +1,8 @@
 import { defineConfig } from 'vite';
-import glsl from 'vite-plugin-glsl';
 import react from '@vitejs/plugin-react';
+import glslify from 'rollup-plugin-glslify';
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    react({
-      include: "**/*.jsx",
-    }),
-    glsl({
-      include: [                   // Glob pattern, or array of glob patterns to import
-        '**/*.glsl', '**/*.wgsl',
-        '**/*.vert', '**/*.frag',
-        '**/*.vs', '**/*.fs'
-      ],
-      exclude: undefined,          // Glob pattern, or array of glob patterns to ignore
-      warnDuplicatedImports: true, // Warn if the same chunk was imported multiple times
-      defaultExtension: 'glsl',    // Shader suffix when no extension is specified
-      compress: false,             // Compress output shader code
-      watch: true,                 // Recompile shader on change
-      root: '/'                    // Directory for root imports
-    })
-  ],
-  server: {
-    hmr: true,
-    port: 3000,
-    watch: {
-        usePolling: true
-    }
-}
+  plugins: [react(), glslify()]
 })
